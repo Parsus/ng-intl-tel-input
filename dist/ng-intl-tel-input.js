08c24657babfec42773859a8bac2041fd8bb5998
Rahul Patel <rahul.patel@parsus.com>
2015-12-03 13:05:57 -0700

    Fix per instance options

--- dist/ng-intl-tel-input.js
+++ dist/ng-intl-tel-input.js
@@ -1,80 +1,100 @@
-angular.module('ngIntlTelInput', []);angular.module('ngIntlTelInput')
-  .provider('ngIntlTelInput', function () {
-    var me = this;
-    var props = {};
-    var setFn = function (obj) {
-      if (typeof obj === 'object') {
-        for (var key in obj) {
-          props[key] = obj[key];
-        }
-      }
-    };
-    me.set = setFn;
-
-    me.$get = ['$log', function ($log) {
-      return Object.create(me, {
-        init: {
-          value: function (elm) {
-            if (!window.intlTelInputUtils) {
-              $log.warn('intlTelInputUtils is not defined. Formatting and validation will not work.');
-            }
-            elm.intlTelInput(props);
-          }
-        },
-      });
-    }];
-  });
+angular.module('ngIntlTelInput', []);
 angular.module('ngIntlTelInput')
+  .provider('ngIntlTelInput', ['lodash', function (_) {
+      var me = this;
+      var props = {};
+      var setFn = function (obj) {
+          if (typeof obj === 'object') {
+              for (var key in obj) {
+                  props[key] = obj[key];
+              }
+          }
+      };
+      me.set = setFn;
+
+      me.$get = ['$log', function ($log) {
+          return Object.create(me, {
+              init: {
+                  value: function (elm, opts) {
+                      if (!window.intlTelInputUtils) {
+                          $log.warn('intlTelInputUtils is not defined. Formatting and validation will not work.');
+                      }
+                      elm.intlTelInput(_.defaults(opts, {}, props));
+                  }
+              },
+          });
+      }];
+  }]);angular.module('ngIntlTelInput')
   .directive('ngIntlTelInput', ['ngIntlTelInput', '$log',
     function (ngIntlTelInput, $log) {
-      return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, elm, attr, ctrl) {
-          // Warning for bad directive usage.
-          if (attr.type !== 'text' || elm[0].tagName !== 'INPUT') {
-            $log.warn('ng-intl-tel-input can only be applied to a *text* input');
-            return;
-          }
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, elm, attr, ctrl) {
+                // Warning for bad directive usage.
+                if (attr.type !== 'text' || elm[0].tagName !== 'INPUT') {
+                    $log.warn('ng-intl-tel-input can only be applied to a *text* input');
+                    return;
+                }
 
-          // Set opts specified with directive attribute
-          if (attr.ngIntlTelInput) {
-              var runtimeOpts = scope.$eval(attr.ngIntlTelInput);
-              ngIntlTelInput.set(runtimeOpts);
-          }
+                var opts = {};
 
-          // Override default country.
-          if (attr.defaultCountry) {
-            ngIntlTelInput.set({defaultCountry: attr.defaultCountry});
-          }
-          // Initialize.
-          ngIntlTelInput.init(elm);
-          // Validation.
-          ctrl.$validators.ngIntlTelInput = function (value) {
-            // if phone number is deleted / empty do not run phone number validation
-            if (value || elm[0].value.length > 0) {
-                return elm.intlTelInput("isValidNumber");
-            } else {
-                return true;
+                // Set opts specified with directive attribute
+                if (attr.ngIntlTelInput) {
+                    opts = scope.$eval(attr.ngIntlTelInput);
+                }
+
+                // Override default country.
+                if (attr.defaultCountry) {
+                    opts.defaultCountry = attr.defaultCountry;
+                }
+                // Initialize.
+                ngIntlTelInput.init(elm, opts);
+                // Validation.
+                ctrl.$validators.ngIntlTelInput = function (value) {
+                    // if phone number is deleted / empty do not run phone number validation
+                    if (value || elm[0].value.length > 0) {
+                        var result = elm.intlTelInput("isValidNumber");
+                        if (result) {
+                            return true;
+                        }
+
+                        if (!opts.allowExtensions) {
+                            return false;
+                        }
+
+                        // Get validation error to see if this is because an extension
+                        // was entered
+                        var error = elm.intlTelInput("isValidNumber")
+
+                        if (error === intlTelInputUtils.validationError.IS_POSSIBLE) {
+                            // Possible valid so considering valid
+                            return true;
+                        }
+
+                        return false;
+
+                    } else {
+                        return true;
+                    }
+                };
+                // Set model value to valid, formatted version.
+                ctrl.$parsers.push(function (value) {
+                    return elm.intlTelInput('getNumber').replace(/[^\d]/, '');
+                });
+                // Set input value to model value and trigger evaluation.
+                ctrl.$formatters.push(function (value) {
+                    if (value) {
+                        if (value.charAt(0) !== '+') {
+                            value = '+' + value;
+                        }
+                        elm.intlTelInput('setNumber', value);
+
+                        // Update value to number formatted by plugin
+                        value = elm.val();
+                    }
+                    return value;
+                });
             }
-          };
-          // Set model value to valid, formatted version.
-          ctrl.$parsers.push(function (value) {
-            return elm.intlTelInput('getNumber').replace(/[^\d]/, '');
-          });
-          // Set input value to model value and trigger evaluation.
-          ctrl.$formatters.push(function (value) {
-            if (value) {
-              if(value.charAt(0) !== '+') {
-                value = '+' + value;
-              }
-              elm.intlTelInput('setNumber', value);
-
-              // Update value to number formatted by plugin
-              value = elm.val();
-            }
-            return value;
-          });
-        }
-      };
+        };
     }]);
--- ng-intl-tel-input.directive.js
+++ ng-intl-tel-input.directive.js
@@ -1,54 +1,74 @@
 angular.module('ngIntlTelInput')
   .directive('ngIntlTelInput', ['ngIntlTelInput', '$log',
     function (ngIntlTelInput, $log) {
-      return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, elm, attr, ctrl) {
-          // Warning for bad directive usage.
-          if (attr.type !== 'text' || elm[0].tagName !== 'INPUT') {
-            $log.warn('ng-intl-tel-input can only be applied to a *text* input');
-            return;
-          }
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, elm, attr, ctrl) {
+                // Warning for bad directive usage.
+                if (attr.type !== 'text' || elm[0].tagName !== 'INPUT') {
+                    $log.warn('ng-intl-tel-input can only be applied to a *text* input');
+                    return;
+                }
 
-          // Set opts specified with directive attribute
-          if (attr.ngIntlTelInput) {
-              var runtimeOpts = scope.$eval(attr.ngIntlTelInput);
-              ngIntlTelInput.set(runtimeOpts);
-          }
+                var opts = {};
 
-          // Override default country.
-          if (attr.defaultCountry) {
-            ngIntlTelInput.set({defaultCountry: attr.defaultCountry});
-          }
-          // Initialize.
-          ngIntlTelInput.init(elm);
-          // Validation.
-          ctrl.$validators.ngIntlTelInput = function (value) {
-            // if phone number is deleted / empty do not run phone number validation
-            if (value || elm[0].value.length > 0) {
-                return elm.intlTelInput("isValidNumber");
-            } else {
-                return true;
+                // Set opts specified with directive attribute
+                if (attr.ngIntlTelInput) {
+                    opts = scope.$eval(attr.ngIntlTelInput);
+                }
+
+                // Override default country.
+                if (attr.defaultCountry) {
+                    opts.defaultCountry = attr.defaultCountry;
+                }
+                // Initialize.
+                ngIntlTelInput.init(elm, opts);
+                // Validation.
+                ctrl.$validators.ngIntlTelInput = function (value) {
+                    // if phone number is deleted / empty do not run phone number validation
+                    if (value || elm[0].value.length > 0) {
+                        var result = elm.intlTelInput("isValidNumber");
+                        if (result) {
+                            return true;
+                        }
+
+                        if (!opts.allowExtensions) {
+                            return false;
+                        }
+
+                        // Get validation error to see if this is because an extension
+                        // was entered
+                        var error = elm.intlTelInput("isValidNumber")
+
+                        if (error === intlTelInputUtils.validationError.IS_POSSIBLE) {
+                            // Possible valid so considering valid
+                            return true;
+                        }
+
+                        return false;
+
+                    } else {
+                        return true;
+                    }
+                };
+                // Set model value to valid, formatted version.
+                ctrl.$parsers.push(function (value) {
+                    return elm.intlTelInput('getNumber').replace(/[^\d]/, '');
+                });
+                // Set input value to model value and trigger evaluation.
+                ctrl.$formatters.push(function (value) {
+                    if (value) {
+                        if (value.charAt(0) !== '+') {
+                            value = '+' + value;
+                        }
+                        elm.intlTelInput('setNumber', value);
+
+                        // Update value to number formatted by plugin
+                        value = elm.val();
+                    }
+                    return value;
+                });
             }
-          };
-          // Set model value to valid, formatted version.
-          ctrl.$parsers.push(function (value) {
-            return elm.intlTelInput('getNumber').replace(/[^\d]/, '');
-          });
-          // Set input value to model value and trigger evaluation.
-          ctrl.$formatters.push(function (value) {
-            if (value) {
-              if(value.charAt(0) !== '+') {
-                value = '+' + value;
-              }
-              elm.intlTelInput('setNumber', value);
-
-              // Update value to number formatted by plugin
-              value = elm.val();
-            }
-            return value;
-          });
-        }
-      };
+        };
     }]);
